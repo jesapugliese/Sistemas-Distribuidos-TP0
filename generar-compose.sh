7d9200fb76f7c510a53e9c5ad61693c99d5fcabf
Jesabel Pugliese <jesabelpugliese004@gmail.com>
2026-03-15 18:56:45 -0300

    feat: add script to generate Docker Compose file for server and configurable number of clients

---
+++ generar-compose.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+NOMBRE_ARCHIVO=$1
+CLIENTES=$2
+
+if [[ $# -ne 2 ]]; then
+    echo "ERROR: Nombre del archivo de salida o número de clientes no proporcionados."
+    echo "Uso: $0 <nombre_archivo> <numero_clientes>"
+    exit 1
+fi
+
+echo "name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net" > $NOMBRE_ARCHIVO
+
+for i in $(seq 1 $CLIENTES)
+do
+    echo "
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server" >> $NOMBRE_ARCHIVO
+done
+
+echo "
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24" >> $NOMBRE_ARCHIVO
+
+echo "Archivo $NOMBRE_ARCHIVO generado con éxito."
