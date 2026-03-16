57192ec53e39054c3c9f9260dcf236881bb7752d
Jesabel Pugliese <jesabelpugliese004@gmail.com>
2026-03-16 15:43:14 -0300

    feat(server): add handler for SIGTERM

--- server/common/server.py
+++ server/common/server.py
@@ -1,5 +1,6 @@
 import socket
 import logging
+import signal
 
 
 class Server:
@@ -8,6 +9,26 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._client_socket = None
+        
+        signal.signal(signal.SIGTERM, self.__sigterm_handler)
+
+    def __sigterm_handler(self, signum, frame):
+        """
+        SIGTERM signal handler
+
+        Function that handles the SIGTERM signal to gracefully 
+        shutdown the server and the current client. 
+        """
+
+        logging.info('action: signal_handler | result: in_progress | signal: SIGTERM')
+        self._server_socket.close()
+        if self._client_socket:
+            self._client_socket.close()
+        
+        logging.info('action: signal_handler | result: success | signal: SIGTERM')
+
+        exit(0)
 
     def run(self):
         """
@@ -18,13 +39,11 @@ class Server:
         finishes, servers starts to accept new connections again
         """
 
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
         while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+            self._client_socket = self.__accept_new_connection()
+            self.__handle_client_connection()
 
-    def __handle_client_connection(self, client_sock):
+    def __handle_client_connection(self):
         """
         Read message from a specific client socket and closes the socket
 
@@ -33,15 +52,15 @@ class Server:
         """
         try:
             # TODO: Modify the receive to avoid short-reads
-            msg = client_sock.recv(1024).rstrip().decode('utf-8')
-            addr = client_sock.getpeername()
+            msg = self._client_socket.recv(1024).rstrip().decode('utf-8')
+            addr = self._client_socket.getpeername()
             logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
             # TODO: Modify the send to avoid short-writes
-            client_sock.send("{}\n".format(msg).encode('utf-8'))
+            self._client_socket.send("{}\n".format(msg).encode('utf-8'))
         except OSError as e:
             logging.error("action: receive_message | result: fail | error: {e}")
         finally:
-            client_sock.close()
+            self._client_socket.close()
 
     def __accept_new_connection(self):
         """
