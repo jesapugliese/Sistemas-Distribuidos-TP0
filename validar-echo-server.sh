78019f189f3d1c0e8972bc411c3033985c4ef173
Jesabel Pugliese <jesabelpugliese004@gmail.com>
2026-03-16 15:15:11 -0300

    fix: correct conditional typo

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -8,7 +8,7 @@ RED="tp0_testing_net"
 RESPUESTA=$(docker run --rm --network "$RED" busybox  \
             sh -c "echo '$MENSAJE' | nc server 12345")
 
-if [[ "$RESPUESTA" == "$MENSAJE" ]]; then
+if [ "$RESPUESTA" = "$MENSAJE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
