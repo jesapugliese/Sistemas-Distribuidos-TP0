6b09ffc021340b11bffaed923180691223dcf61a
Jesabel Pugliese <jesabelpugliese004@gmail.com>
2026-03-20 17:32:29 -0300

    refactor(client): extract client initialization from main

--- client/common/client.go
+++ client/common/client.go
@@ -1,103 +1,120 @@
 package common
 
 import (
-	"bufio"
 	"fmt"
-	"net"
-	"time"
 	"os"
-	"os/signal"
-	"syscall"
+	"strings"
+	"time"
 
 	"github.com/op/go-logging"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
 )
 
 var log = logging.MustGetLogger("log")
 
-// ClientConfig Configuration used by the client
 type ClientConfig struct {
 	ID            string
 	ServerAddress string
-	LoopAmount    int
-	LoopPeriod    time.Duration
 }
 
-// Client Entity that encapsulates how
 type Client struct {
 	config ClientConfig
-	conn   net.Conn
+	// TODO: agencia_de_loteria AgenciaDeLoteria
 }
 
-// NewClient Initializes a new client receiving the configuration
-// as a parameter. It also initializes the signal handler to gracefully 
-// shutdown the client when SIGTERM is received
-func NewClient(config ClientConfig) *Client {
-	client := &Client{
-		config: config,
+type ClientInterface interface {
+	Start() error
+}
+
+// InitConfig Function that uses viper library to parse configuration parameters.
+func InitConfig() (*viper.Viper, error) {
+	v := viper.New()
+
+	v.AutomaticEnv()
+	v.SetEnvPrefix("cli")
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	v.BindEnv("id")
+	v.BindEnv("server", "address")
+	v.BindEnv("loop", "period")
+	v.BindEnv("loop", "amount")
+	v.BindEnv("log", "level")
+
+	v.SetConfigFile("./config.yaml")
+	if err := v.ReadInConfig(); err != nil {
+		fmt.Printf("Configuration could not be read from config file. Using env variables instead")
 	}
-	return client
+
+	if _, err := time.ParseDuration(v.GetString("loop.period")); err != nil {
+		return nil, errors.Wrapf(err, "Could not parse CLI_LOOP_PERIOD env var as time.Duration.")
+	}
+
+	return v, nil
 }
 
-// CreateClientSocket Initializes client socket. In case of
-// failure, error is printed in stdout/stderr and exit 1
-// is returned
-func (c *Client) createClientSocket() error {
-	conn, err := net.Dial("tcp", c.config.ServerAddress)
+// InitLogger Receives the log level to be set in go-logging as a string. This method
+// parses the string and set the level to the logger. If the level string is not
+// valid an error is returned
+func InitLogger(logLevel string) error {
+	baseBackend := logging.NewLogBackend(os.Stdout, "", 0)
+	format := logging.MustStringFormatter(
+		`%{time:2006-01-02 15:04:05} %{level:.5s}     %{message}`,
+	)
+	backendFormatter := logging.NewBackendFormatter(baseBackend, format)
+
+	backendLeveled := logging.AddModuleLevel(backendFormatter)
+	logLevelCode, err := logging.LogLevel(logLevel)
 	if err != nil {
-		log.Criticalf(
-			"action: connect | result: fail | client_id: %v | error: %v",
-			c.config.ID,
-			err,
-		)
+		return err
 	}
-	c.conn = conn
+	backendLeveled.SetLevel(logLevelCode, "")
+
+	logging.SetBackend(backendLeveled)
 	return nil
 }
 
-// StartClientLoop Send messages to the client until some time threshold is met
-func (c *Client) StartClientLoop() {
-	signalChannel := make(chan os.Signal, 1)
-	signal.Notify(signalChannel, syscall.SIGTERM)
+// PrintConfig Print all the configuration parameters of the program.
+// For debugging purposes only
+func PrintConfig(v *viper.Viper) {
+	log.Infof("action: config | result: success | client_id: %s | server_address: %s | loop_amount: %v | loop_period: %v | log_level: %s",
+		v.GetString("id"),
+		v.GetString("server.address"),
+		v.GetInt("loop.amount"),
+		v.GetDuration("loop.period"),
+		v.GetString("log.level"),
+	)
+}
 
-	// There is an autoincremental msgID to identify every message sent
-	// Messages if the message amount threshold has not been surpassed
-	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		select {
-		case <-signalChannel:
-			log.Infof("action: sigterm_received | result: success | client_id: %v", c.config.ID)
-			return
-		default:
-		}
-		
-		// Create the connection the server in every loop iteration. Send an
-		c.createClientSocket()
-
-		// TODO: Modify the send to avoid short-write
-		fmt.Fprintf(
-			c.conn,
-			"[CLIENT %v] Message N°%v\n",
-			c.config.ID,
-			msgID,
-		)
-		msg, err := bufio.NewReader(c.conn).ReadString('\n')
-		c.conn.Close()
-
-		if err != nil {
-			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
-			return
-		}
-
-		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-			c.config.ID,
-			msg,
-		)
-
-		// Wait a time between sending one message and the next one
-		time.Sleep(c.config.LoopPeriod)
-
+// NewClient Initializes a new client. This method is responsible for
+// initializing the configuration and the logger.
+func NewClient() *Client {
+	v, err := InitConfig()
+	if err != nil {
+		log.Criticalf("%s", err)
 	}
-	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
+
+	if err := InitLogger(v.GetString("log.level")); err != nil {
+		log.Criticalf("%s", err)
+	}
+
+	PrintConfig(v)
+
+	config := ClientConfig{
+		ServerAddress: v.GetString("server.address"),
+		ID:            v.GetString("id"),
+	}
+
+	return &Client{config: config}
+}
+
+func (c *Client) Start() error {
+	// TODO:
+	// 1. Crear un socket para el cliente
+	// 2. Crear la apuesta
+	// 3. Crear un sender para que la agencia de loteria pueda enviarle
+	//    al servidor el mensaje con la apuesta
+	// 4. Crear un receiver para que la agencia de loteria pueda recibir
+	//    del servidor el mensaje con el resultado de la apuesta
+	return nil
 }
