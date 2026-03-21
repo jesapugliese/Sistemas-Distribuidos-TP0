c518f47d1a066a5d173dbd33f988e0d23a6b3061
Jesabel Pugliese <jesabelpugliese004@gmail.com>
2026-03-20 21:06:38 -0300

    refactor(server): extract server initialization from main

---
+++ server/common/central_de_loteria.py
@@ -0,0 +1,10 @@
+from utils import Bet
+
+
+class CentralDeLoteriaNacional:
+    def __init__(self):
+        pass
+    
+    # TODO: recibir_apuesta(self, server_protocol): Bet
+
+    # TODO: enviar_resultado(self, server_protocol):
--- server/common/server.py
+++ server/common/server.py
@@ -1,86 +1,52 @@
-import socket
+from configparser import ConfigParser
 import logging
-import signal
+import os
+from central_de_loteria import CentralDeLoteriaNacional
+from communication.server_protocol import ServerProtocol
 
 
 class Server:
-    def __init__(self, port, listen_backlog):
-        # Initialize server socket
-        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._server_socket.bind(('', port))
-        self._server_socket.listen(listen_backlog)
-        self._server_socket.settimeout(0.5)
-        self._client_socket = None
-        
-        signal.signal(signal.SIGTERM, self.__sigterm_handler)
+    def __init__(self):
+        config_params = self._initialize_config()
+        logging_level = config_params["logging_level"]
+        port = config_params["port"]
+        listen_backlog = config_params["listen_backlog"]
 
-    def __sigterm_handler(self, signum, frame):
+        self._initialize_log(logging_level)
+
+        logging.debug(f"action: config | result: success | port: {port} | "
+                      f"listen_backlog: {listen_backlog} | "
+                      f"logging_level: {logging_level}")
+
+        self._port = port
+        self._listen_backlog = listen_backlog
+        self._central_de_loteria = CentralDeLoteriaNacional()
+
+    def start(self):
+        # server_protocol = ServerProtocol(self._port, self._listen_backlog)
+        # self._central_de_loteria.recibir_apuesta(server_protocol)
+        # self._central_de_loteria.enviar_resultado(server_protocol)
+        pass
+
+    def _initialize_config(self):
+        config = ConfigParser()
+        config.read(os.path.join(os.path.dirname(__file__), "config.ini"))
+        return {
+            "logging_level": getattr(logging, config["DEFAULT"]["LoggingLevel"]
+                                     .upper()),
+            "port": int(config["DEFAULT"]["Port"]),
+            "listen_backlog": int(config["DEFAULT"]["ListenBacklog"])
+        }
+
+    def _initialize_log(self, logging_level):
         """
-        SIGTERM signal handler
+        Python custom logging initialization
 
-        Function that handles the SIGTERM signal to gracefully 
-        shutdown the server and the current client. 
+        Current timestamp is added to be able to identify in docker
+        compose logs the date when the log has arrived
         """
-
-        logging.info('action: signal_handler | result: in_progress | signal: SIGTERM')
-        self._server_socket.close()
-        if self._client_socket:
-            self._client_socket.close()
-        
-        logging.info('action: signal_handler | result: success | signal: SIGTERM')
-
-        exit(0)
-
-    def run(self):
-        """
-        Dummy Server loop
-
-        Server that accept a new connections and establishes a
-        communication with a client. After client with communucation
-        finishes, servers starts to accept new connections again
-        """
-
-        while True:
-            self._client_socket = self.__accept_new_connection()
-            if not self._client_socket:
-                continue
-            self.__handle_client_connection()
-
-    def __handle_client_connection(self):
-        """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
-        try:
-            # TODO: Modify the receive to avoid short-reads
-            msg = self._client_socket.recv(1024).rstrip().decode('utf-8')
-            addr = self._client_socket.getpeername()
-            logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
-            # TODO: Modify the send to avoid short-writes
-            self._client_socket.send("{}\n".format(msg).encode('utf-8'))
-        except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
-        finally:
-            self._client_socket.close()
-
-    def __accept_new_connection(self):
-        """
-        Accept new connections
-
-        Function blocks until a connection to a client is made.
-        Then connection created is printed and returned
-        """
-
-        # Connection arrived
-        try:
-            logging.info('action: accept_connections | result: in_progress')
-            c, addr = self._server_socket.accept()
-            logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
-        except socket.timeout:
-            return None
-        except OSError:
-            return None
-
-        return c
+        logging.basicConfig(
+            format='%(asctime)s %(levelname)-8s %(message)s',
+            level=logging_level,
+            datefmt='%Y-%m-%d %H:%M:%S',
+        )
