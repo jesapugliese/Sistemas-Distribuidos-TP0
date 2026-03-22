1526706455605e51353237ce7faa42c3f92cc190
Jesabel Pugliese <jesabelpugliese004@gmail.com>
2026-03-21 22:08:24 -0300

    fix(server): update client handling loop to allow continuous connections

--- server/common/server.py
+++ server/common/server.py
@@ -41,14 +41,12 @@ class Server:
         This repeats until reaching 5 connections.
         """
 
-        clients_handled = 0
-        while clients_handled < 5:
+        while True:
             client_socket = self._accept_new_connection()
             if not client_socket:
                 continue
             self._server_protocol.update_client_socket(client_socket)
             self._handle_client_connection()
-            clients_handled += 1
 
     def _handle_client_connection(self):
         """
